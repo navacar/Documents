793e7c1aa68e6eb5292410619071cf907e63b3fd
navacar <55153165+navacar@users.noreply.github.com>
2020-08-02 00:08:02 +0200

    Updates

---
+++ precache-manifest.825edbe56c2b04c8e5fd9e58a57cac97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e421e565a8098fc8062dd9c3d12a6c71",
+    "url": "/TestTask/index.html"
+  },
+  {
+    "revision": "db27995009c0586ee38a",
+    "url": "/TestTask/static/css/main.d3576372.chunk.css"
+  },
+  {
+    "revision": "3be0e1b05030d3c02d21",
+    "url": "/TestTask/static/js/2.cd0c1f85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestTask/static/js/2.cd0c1f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db27995009c0586ee38a",
+    "url": "/TestTask/static/js/main.5dfc2be5.chunk.js"
+  },
+  {
+    "revision": "fd337bbdb3414311cf4c",
+    "url": "/TestTask/static/js/runtime-main.b9979cf6.js"
+  }
+]);
